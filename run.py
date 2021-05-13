069cd81540671a9ed99a2ac75b08bfe6f46a657f
iRTEX Canonical <irtexwebdev@gmail.com>
2021-05-13 15:17:40 +0300

    Update

---
+++ run.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder="./templates/",  static_url_path='', static_folder='./')
+
+@app.route("/pay")
+def pay():
+    return ({'result': {'message': "tets"}})
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/privacy_policy")
+def privacy_policy():
+    return render_template('privacy_policy.html')
+
+@app.route("/handle_privacy")
+def handle_privacy():
+    return render_template('handle_privacy.html')
+
+app.run(host="0.0.0.0", port=8081)
